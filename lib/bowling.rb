9c0fd25807a0c20380d78ecd1e6b5999dfeac6b2
Chris Olstrom <chris@olstrom.com>
2015-06-26 11:47:11 -0700

    lib/bowling.rb: More expressive code, less magic math.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -12,22 +12,35 @@ class BowlingGame
     frame_index = 0
     (0..9).each do |frame|
       if strike? frame_index
-        score += 10 + @rolls[frame_index + 1] + @rolls[frame_index + 2]
+        score += 10 + strike_bonus(frame_index)
         frame_index += 1
         next
       end
 
       if spare? frame_index
-        score += 10 + @rolls[frame_index + 2]
+        score += 10 + spare_bonus(frame_index)
       else
-        score += @rolls[frame_index] + @rolls[frame_index + 1]
+        score += points_in_frame(frame_index)
       end
+
       frame_index += 2
     end
     score
   end
 
   private
+
+  def points_in_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
+  def spare_bonus(frame_index)
+    @rolls[frame_index + 2]
+  end
+
+  def strike_bonus(frame_index)
+    @rolls[frame_index + 1] + @rolls[frame_index + 2]
+  end
 
   def spare?(frame_index)
     (@rolls[frame_index] + @rolls[frame_index]) == 10
