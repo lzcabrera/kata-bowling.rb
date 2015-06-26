c424110e8133b56a740044235f7cf584e7a75213
Chris Olstrom <chris@olstrom.com>
2015-06-26 11:39:31 -0700

    spec/bowling_spec.rb: Add test for one strike.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -24,4 +24,16 @@ describe BowlingGame do
     subject { game.score }
     it { is_expected.to eq 16 }
   end
+
+  describe 'one strike' do
+    before do
+      game.roll 10
+      game.roll 3
+      game.roll 4
+      16.times { game.roll 0 }
+    end
+
+    subject { game.score }
+    it { is_expected.to eq 24 }
+  end
 end
