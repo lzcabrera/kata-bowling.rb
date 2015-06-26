8b095686f7f501da214240ec940f7480fd93fe4d
Chris Olstrom <chris@olstrom.com>
2015-06-26 10:25:58 -0700

    spec/bowling_spec.rb: A gutter game should score 0.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,5 +2,7 @@ describe BowlingGame do
   describe 'a gutter game' do
     game = BowlingGame.new
     20.times { game.roll 0 }
+    subject { game.score }
+    it { is_expected.to eq 0 }
   end
 end
