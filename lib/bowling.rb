feaae9b2259585a1ffe749b57dcf02b841296bec
Chris Olstrom <chris@olstrom.com>
2015-06-26 11:24:48 -0700

    lib/bowling.rb: Add spare support to #score

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -12,7 +12,11 @@ class BowlingGame
     frame_index = 0
     (0..9).each do |frame|
       frame_points = @rolls[frame_index] + @rolls[frame_index + 1]
-      score += frame_points
+      if frame_points == 10
+        score += 10 + @rolls[frame_index + 2]
+      else
+        score += frame_points
+      end
       frame_index += 2
     end
     score
