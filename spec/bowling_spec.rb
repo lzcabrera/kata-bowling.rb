28128d5b32fde858354ecb7cede7aae2f018f664
Chris Olstrom <chris@olstrom.com>
2015-06-26 10:49:23 -0700

    spec/bowling_spec.rb: Refactor tests.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,14 +1,14 @@
 describe BowlingGame do
+  let(:game) { BowlingGame.new }
+
   describe 'a gutter game' do
-    game = BowlingGame.new
-    20.times { game.roll 0 }
+    before { 20.times { game.roll 0 } }
     subject { game.score }
     it { is_expected.to eq 0 }
   end
 
   describe 'all ones' do
-    game = BowlingGame.new
-    20.times { game.roll 1 }
+    before { 20.times { game.roll 1 } }
     subject { game.score }
     it { is_expected.to eq 20 }
   end
